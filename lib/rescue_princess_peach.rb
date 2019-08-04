3ce5557e69f9fdd27f4ca0a6fe0d1d062f782b36
Patrick Harrison <jahman@bellatlantic.net>
2019-08-04 18:42:38 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,11 @@
+#require 'pry'
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+  #binding.pry
 end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
+ rescue_princess_peach
 # puts status
